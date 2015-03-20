95c6dcebf7ebca69921317fd4b01817c6726e686
Edward Wenger <edwenger@users.noreply.github.com>
2015-03-20 08:41:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='genepi',
       version='0.1',
       description='A genetic epidemiological model of P.falciparum malaria',
-      url='https://github.com/edwenger/ParasiteGenetics/tree/master/genepi',
+      url='https://github.com/edwenger/genepi',
       author='Edward Wenger',
       author_email='ewenger@intven.com',
       packages=['genepi'],
