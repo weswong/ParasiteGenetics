3bb3cc5ad24138c44fbf9b26637c9a01301e0631
Edward Wenger <wenger.edward@gmail.com>
2015-04-15 23:57:00 -0700

    Framework for IBD analysis of model output
    Format .npz model genome output as PLINK format (.map/.ped) for input
    into GERMLINE IBD finder.

--- genepi/analysis/genome.py
+++ genepi/analysis/genome.py
@@ -1,7 +1,11 @@
+import os
+import csv
 import sys
 import subprocess
 import numpy as np
 import pandas as pd
+
+from genepi.genome import bp_per_morgan
 
 def plink_format(genomes):
     '''
@@ -14,6 +18,22 @@ def plink_format(genomes):
        Genetic distance (morgans)
        Base-pair position (bp units)
 
+    '''
+
+    def parse_SNP_id(id):
+        '''
+        Parse SNP ID from dataframe into .map formatted line:
+        e.g. Pf.1.2402 --> 1 Pf.1.2402 0.0016013 2402
+        '''
+        _,chrom,pos=id.split('.')
+        return "{0} {1} {2:.7f} {3}".format(chrom,id,float(pos)/bp_per_morgan,pos)
+
+    with open('g.map','w') as f:
+        for id in genomes.columns:
+            line=parse_SNP_id(id)
+            f.write(line+'\n')
+
+    '''
     The PED file is a white-space (space or tab) delimited file;
     the first six columns are mandatory:
        Family ID
@@ -31,7 +51,33 @@ def plink_format(genomes):
     missing (i.e. 0) or neither.
 
     '''
-    print(genomes[:10])
+
+    def parse_genome(id,genome):
+        '''
+        Parse genome ID and genome from dataframe into .ped formatted line:
+        e.g. 0,00011... --> 0 g0 0 0 0 -9 1 1 1 1 1 1 2 2 2 2  ...
+        '''
+        return '0 g%d 0 0 0 -9 '%id + ' '.join([' '.join([str(x+1)]*2) for x in genome])
+
+    with open('g.ped','w') as f:
+        for id_genome in zip(genomes.index,genomes.values):
+            line=parse_genome(*id_genome)
+            f.write(line+'\n')
+
+    '''
+    df=genomes.applymap(lambda x:str(x+1)*2)
+
+    df.insert(0,'familyID',0)
+    df.insert(1,'individualID',df.index.map(lambda x: 'g'+str(x)))
+    df.insert(2,'paternalID',0)
+    df.insert(3,'maternalID',0)
+    df.insert(4,'sex',0)
+    df.insert(5,'phenotype',-9)
+
+    df.to_csv('plink.ped',sep=' ',
+              header=False,index=False,
+              quoting=csv.QUOTE_NONE,escapechar=' ') # hmm... picky about whitespace?
+    '''
 
 def ibd_finder():
     '''
@@ -39,7 +85,20 @@ def ibd_finder():
     http://www.cs.columbia.edu/~gusev/germline/
     to find pairwise IBD segments
     '''
-    pass
+    with open('test.run','r') as f:
+        try:
+            subprocess.check_call(['./germline',
+                                   #'-haploid',
+                                   #'-bin_out',
+                                   '-min_m','5',
+                                   '-bits','32',
+                                   '-err_hom','0',
+                                   '-err_het','0'],
+                                   stdin=f)
+        except subprocess.CalledProcessError as e:
+            codes={  1:'No IBD segments found',
+                   -11:'Failed'}
+            print('\nERROR: '+codes.get(e.returncode,'code=%d'%e.returncode))
 
 def cluster_finder():
     '''
@@ -49,7 +108,7 @@ def cluster_finder():
     '''
     pass
 
-def genome_analysis(file='simulations/GenomeReport.npz'):
+def genome_analysis(file='simulations/GenomeReport.npz',reformat=True):
     '''
     Analysis of the GenomeReport output
     '''
@@ -61,9 +120,17 @@ def genome_analysis(file='simulations/GenomeReport.npz'):
         sys.exit(e)
 
     genomes=pd.DataFrame(A,columns=header)
-    plink_format(genomes)
+
+    cwd=os.path.dirname(os.path.realpath(__file__))
+    has_file=lambda f: os.path.isfile(os.path.join(cwd,f))
+    if reformat or not (has_file('g.map') and has_file('g.ped')):
+        # TODO: understand why this is crashing for large inputs
+        plink_format(genomes.iloc[-100:,:])
+
     ibd_finder()
-    cluster_finder()
+
+    #cluster_finder()
 
 if __name__ == '__main__':
-    genome_analysis('../../examples/simulations/GenomeReport.npz')
+    genome_analysis('../../examples/simulations/GenomeReport.npz',
+                    reformat=True)
